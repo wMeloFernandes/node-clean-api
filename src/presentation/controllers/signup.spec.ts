00f489f310de05d0a8a3e0648e57162634ae9db7
Will Melo <w.melo.fernandes.un@gmail.com>
2022-03-08 19:36:00 -0300

    test: ensure SignUpController return an error if no email ir provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statudCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'name',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statudCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
