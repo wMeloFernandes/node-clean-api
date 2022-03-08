06fe43534b4fda3291fd667bc68ea8201b16615e
Will Melo <w.melo.fernandes.un@gmail.com>
2022-03-08 19:35:55 -0300

    feat: ensure SignUpController return an error if no email ir provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,15 @@
 export class SignUpController {
   handle (httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        statudCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
     return {
       statudCode: 400,
-      body: new Error('Missing param: name')
+      body: new Error('Missing param: email')
     }
   }
 }
