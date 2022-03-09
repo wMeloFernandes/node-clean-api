ee5bacc12639bd383d510ba5e81989908cd99450
Will Melo <w.melo.fernandes.un@gmail.com>
2022-03-08 21:06:10 -0300

    refactor: add index.ts for errors

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,4 @@
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { MissingParamError } from '../errors/missing-param-error'
-import { ServerError } from '../errors/server-error'
+import { MissingParamError, InvalidParamError, ServerError } from '../errors/'
 import { badRequest, internalServerError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
