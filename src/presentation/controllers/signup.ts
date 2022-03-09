917b9c8fb8fde7fb0a9f5de0e97c9cb2e096c18c
Will Melo <w.melo.fernandes.un@gmail.com>
2022-03-09 10:43:32 -0300

    feat: ensure password confirmation is valid

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -9,13 +9,19 @@ export class SignUpController implements Controller {
 
   handle (httpRequest: HttpRequest): HttpReponse {
     try {
-      const fields = ['name', 'email', 'password', 'password_confirmation']
+      const { email, password, passwordConfirmation } = httpRequest.body
+      const fields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of fields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
