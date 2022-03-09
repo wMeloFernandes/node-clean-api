497a58b8318d05f61e887bdea9c63ee5e1795f65
Will Melo <w.melo.fernandes.un@gmail.com>
2022-03-09 18:14:34 -0300

    feat: ensure EmailValidatorAdapter works

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,27 @@
+import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+describe('EmailValidatorAdapter', () => {
+  test('should return false if validator returns false', () => {
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@gmail.com')
+    expect(isValid).toBe(false)
+  })
+
+  test('should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
+
+  test('should call validator with correct email', () => {
+    const sut = makeSut()
+    const isValidSpy = jest.spyOn(sut, 'isValid')
+    sut.isValid('valid_email@mail.com')
+    expect(isValidSpy).toHaveBeenCalledWith('valid_email@mail.com')
+  })
+})
