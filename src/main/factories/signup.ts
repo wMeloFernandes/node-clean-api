ebc9dd66de68f057750f32b96a076febb2714a2b
Will Melo <w.melo.fernandes.un@gmail.com>
2022-03-10 03:31:53 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,13 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BCryptAdapter } from '../../infra/cryptography/bcryt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const encryptAdapter = new BCryptAdapter(12)
+  const addAccountRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(encryptAdapter, addAccountRepository)
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
