eaa2d37403e795cece4e71d766ad400529fa4ca3
Will Melo <w.melo.fernandes.un@gmail.com>
2022-03-10 06:29:04 -0300

    chore: add module alias

--- jest.config.js
+++ jest.config.js
@@ -13,5 +13,8 @@ module.exports = {
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  preset: '@shelf/jest-mongodb'
+  preset: '@shelf/jest-mongodb',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 }
